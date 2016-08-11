a3a3c0ba6c35c149edc630799b7083413342d59a
leihang <leihangsupport@163.com>
2016-08-11 17:42:06 +0800

    springboot代码

--- utils/src/main/java/cn/code/utils/httpclient/common/HttpTools.java
+++ utils/src/main/java/cn/code/utils/httpclient/common/HttpTools.java
@@ -1,26 +1,39 @@
 package cn.code.utils.httpclient.common;
 
 import com.alibaba.fastjson.JSON;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.params.ConnManagerParams;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.SSLContext;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
@@ -100,6 +113,45 @@ public class HttpTools {
     }
 
     /**
+     * 发送POST请求,发送附件
+     *
+     * @param url       url地址
+     * @param params    参数map：Map<String, String>
+     * @param fileLists 附件
+     * @return 返还post请求原始的response
+     */
+    public static HttpResponse doPost(String url, Map<String, String> params, List<File> fileLists) {
+        CloseableHttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(defaultRequestConfig).build();
+        HttpPost httpPost = new HttpPost(url);
+        CloseableHttpResponse response = null;
+        try {
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            for (String key : params.keySet()) {
+                builder.addPart(key, new StringBody(params.get(key), ContentType.TEXT_PLAIN));
+            }
+            for (File file : fileLists) {
+                FileBody fileBody = new FileBody(file);
+                builder.addPart("files", fileBody);
+            }
+            HttpEntity httpEntity = builder.build();
+            httpPost.setEntity(httpEntity);
+            response = httpClient.execute(httpPost);
+        } catch (IOException e) {
+            LOGGER.error("http client doPost error,url is {},params is {},error is ", url, JSON.toJSON(params), e);
+        } finally {
+            if (response != null) {
+                try {
+                    EntityUtils.consume(response.getEntity());
+                } catch (IOException e) {
+                    LOGGER.error("IOException:", e);
+                }
+            }
+        }
+        return response;
+    }
+
+
+    /**
      * 发送GET请求
      *
      * @param url     url地址
@@ -132,7 +184,7 @@ public class HttpTools {
         return response;
     }
 
-    //TODO:上传文件(单个，多个)，下载文件，
+
 
     /**
      * 封装参数POST
