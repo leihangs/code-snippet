8987b29d6bb6c69da997d906bbb5ee73eb4e3dae
hang.lei <leihangsupport@163.com>
2017-07-14 11:51:20 +0800

    获取系统变量信息

--- java/demo/src/main/java/cn/code/demo/property/EnvTest.java
+++ java/demo/src/main/java/cn/code/demo/property/EnvTest.java
@@ -1,11 +1,16 @@
 package cn.code.demo.property;
 
+import java.util.Map;
+
 /**
  * Created by hang.lei on 2017/7/14.
  */
 public class EnvTest {
 
     public static void main(String[] args) {
-
+        Map<String, String> env = System.getenv();
+        for(Map.Entry<String,String> entry :env.entrySet()){
+            System.out.println(entry.getKey()+":"+entry.getValue());
+        }
     }
 }
